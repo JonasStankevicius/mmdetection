da9aae72c401ef3e3681993302fe562d678df64d
Jonas Stankevičius <jonasstankevicius.js@gmail.com>
2023-02-08 20:14:51 +0200

    Add tutorial model test

---
+++ tools/MMDet_InstanceSeg_Tutorial_test_model.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+from PIL import Image
+import mmcv
+from mmcv.ops import get_compiling_cuda_version, get_compiler_version
+import mmdet
+import torch
+import torchvision
+import os.path as osp
+from mmcv import Config
+from mmdet.apis import set_random_seed
+from mmdet.datasets import build_dataset
+from mmdet.models import build_detector
+from mmdet.apis import train_detector
+from mmdet.apis import inference_detector, show_result_pyplot
+import PIL.ImageDraw as ImageDraw
+
+import mmcv
+from mmcv.runner import load_checkpoint
+
+from mmdet.apis import inference_detector, show_result_pyplot
+from mmdet.models import build_detector
+
+# Choose to use a config and initialize the detector
+config = 'configs/mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco.py'
+# Setup a checkpoint file to load
+checkpoint = 'checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
+
+# Set the device to be used for evaluation
+device = 'cuda:0'
+
+# Load the config
+config = mmcv.Config.fromfile(config)
+# Set pretrained to be None since we do not need pretrained model here
+config.model.pretrained = None
+
+# Initialize the detector
+model = build_detector(config.model)
+
+# Load checkpoint
+checkpoint = load_checkpoint(model, checkpoint, map_location=device)
+
+# Set the classes of models for inference
+model.CLASSES = checkpoint['meta']['CLASSES']
+
+# We need to set the model's cfg for inference
+model.cfg = config
+
+# Convert the model to GPU
+model.to(device)
+# Convert the model into evaluation mode
+model.eval()
+
+# Use the detector to do inference
+img = 'demo/demo.jpg'
+result = inference_detector(model, img)
+
+# Let's plot the result
+show_result_pyplot(model, img, result, score_thr=0.3)
